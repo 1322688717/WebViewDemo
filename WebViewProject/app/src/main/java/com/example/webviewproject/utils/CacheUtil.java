f15d301f7d9eac8b85c0900dbf8bafbb3a8b6e57
1322688717 <81673615+1322688717@users.noreply.github.com>
2022-10-03 21:50:00 +0800

    增加工具列和自定义控件

---
+++ WebViewProject/app/src/main/java/com/example/webviewproject/utils/CacheUtil.java
@@ -0,0 +1,35 @@
+package com.example.webviewproject.utils;
+
+import android.content.Context;
+import android.os.Environment;
+
+import java.io.File;
+
+/**
+ * @ClassName: CacheUtil
+ * @Description:
+ */
+public class CacheUtil {
+    /***
+     */
+    public static void clearAllCache(Context context) {
+        deleteDir(context.getCacheDir());
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            deleteDir(context.getExternalCacheDir());
+        }
+    }
+
+    private static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+}
